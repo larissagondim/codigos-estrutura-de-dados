5232ee542984271c3cfecc68af5fff1e16c8e532
larissagondim <gondimvilasboaslarissa@gmail.com>
2026-04-29 11:19:45 -0300

    fix: overflow error in ascii table code

--- aula1/tabela-ascii/tabela_ASCII.c
+++ aula1/tabela-ascii/tabela_ASCII.c
@@ -7,9 +7,10 @@ void mostrar_tabela_ascii(void) {
     printf("----------------------\n");
     printf("DEC | CHAR | HEX | BIN\n");
     printf("----------------------\n");
-
-    for (unsigned char i = 0; i < 255; i++) {
+    unsigned char i;
+    for (i = 0; i < 254; i++) {
         if (isprint(i)) printf("%3d | %4c | %#2x | %b\n", i, i, i, i);
         else printf("%3d |      | %#2x | %b\n", i, i, i);
     }
+    printf("%3d | %4c | %#2x | %b\n", i, i, i, i);
 }
