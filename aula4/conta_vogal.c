ef74990d1d376ae23759f01930d741c47e86907b
larissagondim <larissaagondim@outlook.com>
2026-05-11 19:08:17 -0300

    feat: new stuff

---
+++ aula4/conta_vogal.c
@@ -0,0 +1,21 @@
+/*                                                                                                                                                                                                                    * Nome do arquivo: conta_vogal.c
+ * Autor: Larissa Gondim
+ * Data: 11/05/2026
+ * Descrição: Programa que conta vogais de um vetor
+ * Versão: 1.0
+ */
+
+// includes:
+// stdio.h -> biblioteca padrao de input e output da linguagem c
+// stdlib.h -> biblioteca padrão de C, importada para utilizar 'malloc'
+// string.h -> biblioteca para mannipulação de string (vetor de caracteres)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// funções auxiliares
+// ========== MAIN ==========
+int main(void) {
+    int n;
+    char *p = malloc(n);
+    return 0;
+}
