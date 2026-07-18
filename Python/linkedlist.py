497463f21a70e24013691816847dcb33824a00af
larissagondim <gondimvilasboaslarissa@gmail.com>
2026-07-17 23:58:11 -0300

    feat: update append method and improve documentation

--- Python/linkedlist.py
+++ Python/linkedlist.py
@@ -10,23 +10,56 @@ class Node:
 
 class LinkedList:
 
+    # ll constructor
+    # initializes a new ll with one node
+    # step-by-step:
+    # -> gets the newer node
+    # -> updates head and tail to point to that new node
+    # -> updates length
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
+    # 'print_list()'
+    # prints all list elements
+    # step-by-step:
+    # -> creates a temporary variable to store the first element of the list (aka head)
+    # -> iterates through it until the next value points to none (temp is updated to temp.next in every iteration)
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
     
+    # 'append()'
     # the append method is going to:
     # - add a new node to the end of the list
+    # step by step:
+    # -> a new node is created, which contains the value that we are going to append
+    # -> if the list is empty:
+    #   -> both head and tail are going to point to the new element
+    # -> else
+    #   -> last element and tail are going to point to the new node
+    # length is updated.
+
     def append(self, value):
+        # new node creation
         new_node = Node(value)
-        self.tail = new_node
+
+        # if list is empty, new node -> whole list
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # list not empty
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        # updated length, which happens in both cases    
+        self.length += 1
+        return True
+
     
     # the prepend method is going to:
     # - add a new node to the beginning
@@ -37,8 +70,9 @@ class LinkedList:
     # - add it to the end of the list
     # def insert(self, index, value);
         
-# using the linked list class
+# using the linked list class and testing
 my_linked_list = LinkedList(4)
+my_linked_list.append(2)
 print(f"Head: {my_linked_list.head.value}")
 print(f"Tail: {my_linked_list.tail.value}")
 print(f"Length: {my_linked_list.length}")
